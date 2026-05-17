104e9f1e5a0e377d85481f753aacd63ca4ec9f03
Josué López <josuerlb1110@gmail.com>
2026-05-17 12:03:04 -0600

    CORS update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const io     = new Server(server, {
 });
 
 const cors = require('cors');
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: 'https://casco-web.vercel.app' }));
 
 // ─── Middleware ───
 app.use(express.json());
